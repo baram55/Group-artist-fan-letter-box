4108ee57c9e1850b29a06926ab95cbc29d7151f7
baram55 <asd7973@gmail.com>
2024-01-31 20:35:30 +0900

    날짜 포맷 완성

--- group-artist-fan-letter-box/src/components/InputForm.jsx
+++ group-artist-fan-letter-box/src/components/InputForm.jsx
@@ -56,11 +56,16 @@ const getFormattedDate = () => {
   const date = new Date();
   const year = date.getFullYear().toString().substring(2, 4);
   const month = (date.getMonth() + 1).toString().padStart(2, "0");
-
   const day = date.getDate();
+  const hour =
+    date.getHours() > 12
+      ? (date.getHours() - 12).toString().padStart(2, "0")
+      : date.getHours().toString().padStart(2, "0");
+  const amPm = date.getHours() < 12 ? "오전" : "오후";
+  const minute = date.getMinutes().toString().padStart(2, "0");
+  const second = date.getSeconds().toString().padStart(2, "0");
 
-  const result = `${year}. ${month}. ${day}. `;
-  console.log(result);
+  return `${year}. ${month}. ${day}. ${amPm} ${hour}:${minute}:${second}`;
 };
 
 const submitHandler = (event) => {
